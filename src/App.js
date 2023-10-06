f803a38eb9ba25b3961dda436d67df931bfb9ba8
Takeshikovacsx <hannerhd1@gmail.com>
2023-10-06 17:56:54 -0500

    Cambios en registro, estilo de registro

--- src/App.js
+++ src/App.js
@@ -32,20 +32,20 @@ function App() {
 
 
 
-                <h1 className='ml-4'>Home</h1>
+                <h1 className='ml-4 font-bold'>Home</h1>
               </Link>
 
 
               <div className='flex'>
 
-                <Link to="/Login" className="bg-indigo-500 hover:bg-indigo-700 text-white px-4 py-2 rounded-md flex items-center">
+                <Link to="/Login" className="bg-indigo-500 hover:bg-indigo-700 text-white  px-4 py-2 rounded-md flex items-center">
                   <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
                   </svg>
                   Login
                 </Link>
 
-                <Link to="/Registro" className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-md opacity-75 hover:opacity-100 ml-4 flex items-center">
+                <Link to="/Registro" className="bg-gray-700 hover:bg-gray-900 text-white  py-2 px-4 rounded-md opacity-75 hover:opacity-100 ml-4 flex items-center">
                   <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"></path>
                   </svg>
@@ -60,8 +60,14 @@ function App() {
         {Home && (
           <section className="bg-blue-500 py-16 text-white">
             <div className="container mx-auto text-center">
-              <h1 className="text-4xl font-bold mb-4">¡Bienvenido a nuestra Aplicación React con Tailwind CSS!</h1>
-              <p className="text-lg mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget ante vel nisi scelerisque fringilla.</p>
+              <h1 className="text-4xl font-bold mb-4">¡Bienvenido a nuestro Gimnasio!</h1>
+              <p className="text-lg mb-8">¡Bienvenido al Gimnasio Vitality!
+
+En Vitality, nuestra pasión es ayudarte a alcanzar tus objetivos de bienestar y a llevar un estilo de vida saludable. Nos enorgullece ser tu socio en este emocionante viaje hacia la salud y el bienestar.
+
+Nuestro gimnasio está diseñado para ofrecerte un espacio inspirador donde puedas desafiar tus límites, superar tus metas y transformar tu cuerpo y mente. Ofrecemos una amplia variedad de equipos de vanguardia, clases dirigidas por expertos y un equipo de entrenadores profesionales que están comprometidos con tu éxito.
+
+Ya sea que estés buscando aumentar tu fuerza, mejorar tu resistencia, perder peso o simplemente mantener un estilo de vida activo, en Vitality encontrarás todo lo que necesitas. Nuestro ambiente acogedor y motivador te inspirará a dar lo mejor de ti en cada entrenamiento.</p>
               <button className="bg-white text-blue-500 hover:bg-blue-100 text-lg font-semibold py-2 px-6 rounded-full transition duration-300 ease-in-out">
                 Empezar
               </button>
--- src/components/Registro.js
+++ src/components/Registro.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
-
 function Registro() {
   // Estados para guardar los valores de los campos
   const [nombre, setNombre] = useState('');
   const [email, setEmail] = useState('');
   const [mensaje, setMensaje] = useState('');
+  const [usuarios, setUsuarios] = useState([]); // Estado para almacenar los usuarios registrados
 
   // Manejadores de eventos para actualizar los estados cuando los campos cambian
   const handleNombreChange = (e) => {
@@ -24,7 +24,15 @@ function Registro() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    //  realizar acciones adicionales, como enviar los datos a un servidor
+    // Crear un nuevo usuario con los datos ingresados
+    const nuevoUsuario = {
+      nombre,
+      email,
+      mensaje,
+    };
+
+    // Agregar el nuevo usuario a la lista de usuarios
+    setUsuarios([...usuarios, nuevoUsuario]);
 
     // Limpia los campos después del envío
     setNombre('');
@@ -33,65 +41,85 @@ function Registro() {
   };
 
   return (
-
     <>
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-          <div className="bg-white p-8 rounded shadow-md w-96">
-            <h2 className="text-2xl font-semibold mb-4">Formulario de Contacto</h2>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <label htmlFor="nombre" className="block text-sm font-medium text-gray-700">
-                  Nombre
-                </label>
-                <input
-                  type="text"
-                  id="nombre"
-                  className="mt-1 p-2 w-full border rounded-md"
-                  placeholder="Nombre completo"
-                  value={nombre}
-                  onChange={handleNombreChange}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  className="mt-1 p-2 w-full border rounded-md"
-                  placeholder="correo@example.com"
-                  value={email}
-                  onChange={handleEmailChange}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="mensaje" className="block text-sm font-medium text-gray-700">
-                  Mensaje
-                </label>
-                <textarea
-                  id="mensaje"
-                  className="mt-1 p-2 w-full border rounded-md"
-                  rows="4"
-                  placeholder="Escribe tu mensaje aquí"
-                  value={mensaje}
-                  onChange={handleMensajeChange}
-                  required
-                ></textarea>
-              </div>
-              <div className="text-center">
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
-                >
-                  Enviar
-                </button>
-              </div>
-            </form>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <svg className="mx-auto h-10 w-auto" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"></path>
+          </svg>
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Registration Form</h2>
+
+          <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
+            
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <label htmlFor="nombre" className="block text-sm font-medium leading-6 text-gray-900">
+                Nombre
+              </label>
+              <input
+                type="text"
+                id="nombre"
+                className="mt-1 p-2 w-full border rounded-md"
+                placeholder="Nombre completo"
+                value={nombre}
+                onChange={handleNombreChange}
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="mt-1 p-2 w-full border rounded-md"
+                placeholder="correo@example.com"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="mensaje" className="block text-sm font-medium leading-6 text-gray-900">
+                Proposito De Inicio
+              </label>
+              <textarea
+                id="mensaje"
+                className="mt-1 p-2 w-full border rounded-md"
+                rows="4"
+                placeholder="Escribe tu mensaje aquí"
+                value={mensaje}
+                onChange={handleMensajeChange}
+                required
+              ></textarea>
+            </div>
+
+            <div className="text-center">
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Enviar
+              </button>
+            </div>
+
+          </form>
+
+          {/* Mostrar la lista de usuarios registrados */}
+          <div className="mt-6">
+            <h3 className="text-xl font-semibold">Usuarios Registrados:</h3>
+            <ul className="list-disc list-inside">
+              {usuarios.map((usuario, index) => (
+                <li key={index}>
+                  Nombre: {usuario.nombre}, Email: {usuario.email}, Mensaje: {usuario.mensaje}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
+      </div>
     </>
   );
 }
